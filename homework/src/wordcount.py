a57c194d6c7197eea3fe6f033d70b7c1b0645383
Paolabustos0510 <pabustos@unal.edu.co>
2025-05-03 10:06:40 -0500

    Fotos

---
+++ homework/src/wordcount.py
@@ -0,0 +1,44 @@
+# obtain a list of files in the input directory
+import os
+
+from .write_count_words import write_count_words
+
+
+def read_all_lines():
+    all_lines = []
+    input_file_list = os.listdir("data/input/")
+    for filename in input_file_list:
+        with open(filename, "r", enconding="utf-8") as f:
+            lines = f.readlines()
+            all_lines.extend(lines)
+    return read_all_lines
+
+
+def main():
+
+    ###
+    input_file_list = os.listdir("data/input/")
+    ##all_lines = read_all_lines()
+
+    ## Lee
+    ## Limpia
+    ## Separa
+    ## Cuenta
+    ## Resultado
+
+    # count the frequency of the words in the files in the input directory
+    counter = {}
+    for filename in input_file_list:
+        with open("data/input/" + filename) as f:
+            for l in f:
+                for w in l.split():
+                    w = w.lower().strip(",.!?")
+                    counter[w] = counter.get(w, 0) + 1
+
+    ###
+    # create the directory output/ if it doesn't exist
+    write_count_words(counter)
+
+
+if __name__ == "__main__":
+    main()
